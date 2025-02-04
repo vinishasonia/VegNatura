de3ea727e8446be110d6718d8c997dc134e868c6
Vinisha <soniavinisha@gmail.com>
2025-02-04 06:30:40 +0530

    Your commit message here

--- GROCERY/main.js
+++ GROCERY/main.js
@@ -3,14 +3,16 @@
 var swiper = new Swiper(".home", {
   spaceBetween: 30,
   centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  // from here
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  // till here
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
