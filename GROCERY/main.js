2f1a8de26b5412aae56d4f124af8d28e58fa7531
Vinisha <soniavinisha@gmail.com>
2025-02-14 09:48:13 +0530

    project completed

--- GROCERY/main.js
+++ GROCERY/main.js
@@ -4,17 +4,23 @@ var swiper = new Swiper(".home", {
   spaceBetween: 30,
   centeredSlides: true,
   // from here
-  // autoplay: {
-  //   delay: 2000,
-  //   disableOnInteraction: false,
-  // },
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   clickable: true,
-  // },
-  // till here
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".navbar");
+
+hamburgerMenu.addEventListener("click", () => {
+  navbar.classList.toggle("show");
 });
